633a32d45a3b4f642d8a1588bf084754907d4e0c
Ian Grace <irg102@hotmail.co.uk>
2017-03-23 13:43:43 +0000

    Modified Game class to keep track of floater.

--- game.py
+++ game.py
@@ -9,13 +9,17 @@ class Game (object):
 			self._players.append(Player(player))
 		self._kitty = 0
 		self._deck = Deck(5)
+		self._floater = []
 
 	def playGame(self):
 		self._deck.newRound()
+		self._floater.append(self._deck.deal())
 		for i in range(5):
 			for player in self._players:
 				player.receiveCard(self._deck.deal())
 		for person in self._players:
 		 	print(person)
-
+		floater = "The floater is: "
+		floater += str(self._floater[0])
+		print(floater)
 			
