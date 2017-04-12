dedab800131de02897b1c0421cbfd157ab34d96c
Ian Grace <irg102@hotmail.co.uk>
2017-04-12 14:53:50 +0100

    Added docstring comments to Deck

--- deck.py
+++ deck.py
@@ -2,16 +2,24 @@ from card import *
 import random
 
 class Deck (object):
+	""" A standard deck of cards of variable size.
 
+	Contains a list of playing cards (the deck) and is aware of its size so reduced decks may be used.
+	"""
 	def __init__(self, size):
+		""" Sets the deck size for the game and creates an empty list (deck) of cards.
+
+		Args:
+			size (int): Number of ranks in a deck. For full deck size==13. For tens to aces size==5. Etc...
+		"""
 		self._size = size
 		self._cards = []
 
-	def clearDeck(self):
+	def _clearDeck(self):
 		while len(self._cards) > 0:
 			self._cards.pop()
 
-	def populate(self):
+	def _populate(self):
 		fullDeck = len(Card.RANK)
 		indexPosition = fullDeck - self._size
 		for i in range(indexPosition, fullDeck):
@@ -19,15 +27,17 @@ class Deck (object):
 				card = Card(Card.RANK[i], suit)
 				self._cards.append(card)
 
-	def shuffle(self):
+	def _shuffle(self):
 		random.shuffle(self._cards)
 
 	def deal(self):
+		""" Removes a card from the deck and returns it. """
 		card = self._cards.pop()
 		return card
 
 	def newRound(self):
-		self.clearDeck()
-		self.populate()
-		self.shuffle()
+		""" Readies a new deck of cards to play another round of the game. """
+		self._clearDeck()
+		self._populate()
+		self._shuffle()
 
