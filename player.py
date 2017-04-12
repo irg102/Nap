b15fc5230bf977fd42b3474cb413aaa0334de7af
Ian Grace <irg102@hotmail.co.uk>
2017-04-12 14:55:05 +0100

    Added docstring comments to Player

--- player.py
+++ player.py
@@ -1,8 +1,21 @@
 from hand import *
 
 class Player (object):
+	""" Creates a generic player to participate in a card game.
 
+	Player has a name, a hand of cards and a value of money.
+
+	Attributes:
+		_playerName (string): Name of player
+		_playerHand (Hand): Hand of cards as defined in hand.py
+		_playerMoney (float): Amount of money won/lost by player.
+	"""
 	def __init__(self, name):
+		""" Sets defaults for a newly created player and sets name.
+
+		Args:
+			name (string): name to be set for player.
+		"""
 		self._playerName = name
 		self._playerHand = Hand()
 		self._playerMoney = 0
@@ -17,5 +30,10 @@ class Player (object):
 		return playerString
 
 	def receiveCard(self, card):
+		""" Append card to the players hand.
+
+		Args:
+			card (Card): Playing card as defined in card.py.
+		"""
 		self._playerHand.addCard(card)
 
