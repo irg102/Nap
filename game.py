b6bfbbbb3de85910edf7689fe67a26ca4e2450ab
Ian Grace <irg102@hotmail.co.uk>
2017-04-12 14:55:33 +0100

    Added docstring comments to Game

--- game.py
+++ game.py
@@ -2,8 +2,23 @@ from deck import *
 from player import *
 
 class Game (object):
+	""" Nap game object.
 
+	Stores game state and handles game logic and rules.
+
+	Attributes:
+		_players (list): A list of the players in the game.
+		_kitty (float): Value of money currently in kitty.
+		_deck (Deck): A deck of playing cards as defined in deck.py.
+		_floater (Card): The floater card for the current round of the game.
+	"""
 	def __init__(self, playerList):
+		""" Sets the defaults for starting a new game.
+
+		Args:
+			playerList (list of strings): Contains the strings of the names of the players to be used when creating 
+											the players for the game.
+		"""
 		self._players = []
 		for player in playerList:
 			self._players.append(Player(player))
@@ -12,6 +27,7 @@ class Game (object):
 		self._floater = []
 
 	def playGame(self):
+		""" Handles game logic for playing the game. """
 		self._deck.newRound()
 		self._floater.append(self._deck.deal())
 		for i in range(5):
