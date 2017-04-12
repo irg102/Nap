be37e9521f643000fd802299fc47e5bf4b35b7ba
Ian Grace <irg102@hotmail.co.uk>
2017-04-12 14:52:47 +0100

    Made member variables read-only. Added docstring comments.

--- card.py
+++ card.py
@@ -1,19 +1,37 @@
 class Card (object):
+
+	""" Standard playing card.
+
+	Creates a playing card with hidden suit and rank attributes.
+	"""
     
 	RANK = ('2', '3', '4', '5', '6', '7', '8', '9', '10', 'J', 'Q', 'K', 'A')
 	SUIT = ('H', 'C', 'D', 'S')
 	
 	def __init__(self, rank, suit):
+
+		""" Initializes the hidden rank and suit attributes.
+
+		Args:
+			rank (RANK): A string of type Card.RANK is expected to initialize card to rank.
+			suit (SUIT): A string of type Card.SUIT is expected to initialize card to suit. 
+		
+		"""
 		self._rank = rank
 		self._suit = suit
 
-	def getRank(self):
+	@property
+	def rank(self):
+		""" Returns read-only rank variable. """
 		return self._rank
 
-	def getSuit(self):
+	@property
+	def suit(self):
+		""" Returns read-only suit variable. """
 		return self._suit
 
 	def __str__(self):
+		""" Returns a unique string for each card. """
 		return self._rank + self._suit
 
 	
